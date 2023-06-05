2e40a4ff71609b07c7c54c613bd0f2a66ab5dc24
Diego <diego.aguirre.9805@gmail.com>
2023-06-05 12:20:01 -0500

    subiendo el repo final

---
+++ schemas/product.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+
+
+class Product(BaseModel):
+    id: Optional[int]=None
+    name : str = Field(max_length=40, min_length=2,description="product name")
+    brand : str = Field(max_length=30, min_length=2,description="brand of the product")
+    description : str =Field(max_length=100, min_length=8,description="product description")
+    price : float = Field(ge=100)
+    entry_date : str = Field(max_length=50, min_length=5,description="product delivery date")
+    availability : str = Field (max_length=3, min_length=1, description="product availability")
+    available_quantity : int = Field (ge=1, le=10000000)
+    class Config:
+        schema_extra = {
+            "example":{
+                "id":1,
+                'name':'submarino',
+                'brand':'marinela',
+                'description':'caramel cream filled cake ',
+                'price':1200,
+                'entry_date':'19/03/2017',
+                'availability':"yes", 
+                'available_quantity':300
+            }
+        }
---
+++ schemas/supplier.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+
+
+class Supplier (BaseModel):
+    id : Optional[int]
+    sub_name : str = Field(max_length=40,min_length=2,description="name supplier")
+    address : str = Field(max_length=40,min_length=2,description="address")
+    phone : int = Field(ge=5)
+    email : str = Field(max_length=40,min_length=2,description="provider email")
+
+    class Config:
+        schema_extra = {
+            "example":{
+                "id":1,
+                "sub_name":"Bimbo",
+                "address":"P. Sherman, calle Wallaby, 42, Sydney",
+                "phone":6017500500,
+                "email":"example123@gmail.com"
+            
+            }
+        }
